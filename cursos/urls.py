3a7182899b48e3ba36a15e2f6bcbaa1a46ed6eed
LimberMorales <javi777.jlmm@gmail.com>
2025-06-23 01:40:22 -0400

    inscripcion de cursos

--- cursos/urls.py
+++ cursos/urls.py
@@ -1,17 +1,25 @@
 from django.urls import path
-
-from cursos.views import inscripcion_list, inscripcion_create, inscripcion_edit, inscripcion_delete, curso_list, \
-    curso_create, curso_edit, curso_delete
+from cursos.views import (
+    inscripcion_list,
+    inscripcion_create,
+    inscripcion_edit,
+    inscripcion_delete,
+    curso_list,
+    curso_create,
+    curso_edit,
+    curso_delete
+)
 
 app_name = 'cursos'
 
 urlpatterns = [
-    #modulo de incripciones
+    # Módulo de inscripciones
     path('inscripciones/', inscripcion_list, name='inscripcion_list'),
     path('inscripciones/crear/', inscripcion_create, name='inscripcion_create'),
     path('inscripciones/editar/<int:pk>/', inscripcion_edit, name='inscripcion_edit'),
     path('inscripciones/eliminar/<int:pk>/', inscripcion_delete, name='inscripcion_delete'),
-    #modulo de cursos
+
+    # Módulo de cursos
     path('cursos/', curso_list, name='curso_list'),
     path('cursos/crear/', curso_create, name='curso_create'),
     path('cursos/editar/<int:pk>/', curso_edit, name='curso_edit'),
